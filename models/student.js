1421b78966d088edf9060a13169905c57d8d4fed
myselfrk <myselfrk0@gmail.com>
2022-10-23 14:33:04 +0000

    issued book made optional

--- models/student.js
+++ models/student.js
@@ -26,7 +26,6 @@ const studentSchema = new mongoose.Schema(
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Book",
-        required: true,
       },
     ],
     soft_deleted: {
