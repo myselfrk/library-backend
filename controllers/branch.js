3c98193a2d8267cbf7af773697d9b73d850c3366
myselfrk <myselfrk0@gmail.com>
2022-10-23 14:23:47 +0000

    Update branch.js

--- controllers/branch.js
+++ controllers/branch.js
@@ -8,10 +8,13 @@ const BookSchema = require("../models/book");
 exports.list = function (req, res) {
   const { search = "" } = request.getFilteringOptions(req, ["search"]);
 
-  Branch.find({
-    branch_name: { $regex: new RegExp(search), $options: "i" },
-    soft_deleted: { $ne: true },
-  })
+  Branch.find(
+    {
+      branch_name: { $regex: new RegExp(search), $options: "i" },
+      soft_deleted: { $ne: true },
+    },
+    ["-soft_deleted"]
+  )
     .sort({ created_at: -1 })
     .exec(function (err, data) {
       if (err) return response.sendNotFound(res);
